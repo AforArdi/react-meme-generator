cb88f15f5b74f2c63945885fcc6e5ad6cf733fd1
AforArdi <binsbro36@gmail.com>
2026-03-28 07:57:56 +0600

    basics added, hedaer added workin on main, static

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,16 @@
+const Main = () => {
+    return ( 
+        <main>
+            <div>
+                <label htmlFor="">
+                    <input type="text" name="topText" id="" />
+                </label>
+                <label htmlFor="">
+                    <input type="text" name="bottomText" id="" />
+                </label>
+            </div>
+        </main>
+     );
+}
+ 
+export default Main;
