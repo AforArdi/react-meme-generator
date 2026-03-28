9a5a0c1f7437a61acfc43b0b8d336de84cd8d616
AforArdi <binsbro36@gmail.com>
2026-03-28 08:37:02 +0600

    Main component added, static

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,13 +1,27 @@
+import { MdImageSearch } from "react-icons/md";
+
 const Main = () => {
     return ( 
         <main>
-            <div>
+            <div className="container mx-auto flex items-center justify-between gap-8 mt-8">
                 <label htmlFor="">Top Text
-                    <input type="text" name="topText" id="" placeholder="Top Text" />
+                    <input type="text" placeholder="Are You" className="input input-neutral" />
                 </label>
                 <label htmlFor="">Bottom Text
-                    <input type="text" name="bottomText" id="" placeholder="Bottom Text" />
+                    <input type="text" placeholder="Dumb?" className="input input-neutral" />
                 </label>
+            </div>
+
+            <div className="container mx-auto flex justify-center mt-6">
+                <button className="btn btn-soft w-full">
+                    Get A Meme <MdImageSearch></MdImageSearch>
+                </button>
+            </div>
+
+            <div className="relative flex flex-col justify-center items-center">
+                <img className="h-auto mx-w-[100%] rounded-md" src="http://i.imgflip.com/1bij.jpg" />
+                <span className="">Walk into Mordor</span>
+                <span  className="top">One does not simply</span>
             </div>
         </main>
      );
