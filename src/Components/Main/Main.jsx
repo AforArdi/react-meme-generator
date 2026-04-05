7231f37ff0b29bca8adf534ed21116f98d035e23
AforArdi <binsbro36@gmail.com>
2026-04-05 23:59:44 +0600

    Tracking onChange form input, VERY IMPORTANT

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,14 +1,40 @@
+import { useState } from "react";
 import { MdImageSearch } from "react-icons/md";
+import '../../App.css'
 
 const Main = () => {
+    const [meme, setMeme] = useState({
+        topText: "One does not simply",
+        bottomText: "Walk into Mordor",
+        imageUrl: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    const handleChange=(e)=>{
+        const {value, name} = e.currentTarget;
+        // console.log(topText);
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+
     return ( 
         <main>
             <div className="container mx-auto flex items-center justify-between gap-8 mt-8">
                 <label htmlFor="">Top Text
-                    <input type="text" placeholder="Are You" className="input input-neutral" />
+                    <input type="text" 
+                    name="topText" 
+                    onChange={handleChange}
+                    value={meme.topText}
+                    placeholder="Are You" 
+                    className="input input-neutral" />
                 </label>
                 <label htmlFor="">Bottom Text
-                    <input type="text" placeholder="Dumb?" className="input input-neutral" />
+                    <input type="text"
+                    name="bottomText"
+                    onChange={handleChange}
+                    value={meme.bottomText}
+                    placeholder="Dumb?" className="input input-neutral" />
                 </label>
             </div>
 
@@ -18,10 +44,10 @@ const Main = () => {
                 </button>
             </div>
 
-            <div className="relative flex flex-col justify-center items-center">
-                <img className="h-auto mx-w-[100%] rounded-md" src="http://i.imgflip.com/1bij.jpg" />
-                <span className="">Walk into Mordor</span>
-                <span  className="top">One does not simply</span>
+            <div className="meme">
+                <img className="h-auto mx-w-[100%] rounded-md" src={meme.imageUrl} />
+                <span className="top">{meme.topText}</span>
+                <span  className="bottom">{meme.bottomText}</span>
             </div>
         </main>
      );
